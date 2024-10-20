539bec4d66dac25fdfb9aec06ef8ee0b02b708a3
robert.s.hogan@icloud.com <robert.s.hogan@icloud.com>
2024-10-20 05:49:51 -0700

    added BaseButton test

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -1,21 +1,21 @@
 // cypress/support/component.js
 
 // Import Cypress commands for mounting Vue components
-import { mount } from "cypress/vue";
+import { mount } from '@cypress/vue' // Corrected import
 
 // Make `cy.mount` available globally to all component tests
-Cypress.Commands.add("mount", mount);
+Cypress.Commands.add('mount', mount)
 
 // Additional configuration or global commands can be added here
-import "../../src/assets/index.css";
+import '../../src/assets/tailwind.css'
 
-import sinon from "sinon";
-Cypress.sinon = sinon;
+import sinon from 'sinon'
+Cypress.sinon = sinon
 
 // Ignore 404 errors for favicon requests
-Cypress.on("fail", (error, runnable) => {
-  if (error.message.includes("favicon.png")) {
-    return false;
+Cypress.on('fail', (error, runnable) => {
+  if (error.message.includes('favicon.png')) {
+    return false
   }
-  throw error;
-});
+  throw error
+})
--- src/components/atoms/BaseButton/BaseButton.vue
+++ src/components/atoms/BaseButton/BaseButton.vue
@@ -1,58 +1,91 @@
 <!-- src/components/atoms/BaseButton.vue -->
 <template>
   <button
-    :class="computedClass"
-    @click="onClick"
+    :class="buttonClasses"
+    @click="handleClick"
     :type="type"
-    :disabled="disabled"
+    :disabled="loading || disabled"
+    data-cy="button"
   >
-    <slot />
+    <div class="flex items-center justify-center space-x-2">
+      <svg
+        v-if="loading"
+        class="animate-spin h-5 w-5 text-white"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <circle
+          class="opacity-25"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="currentColor"
+          stroke-width="4"
+        ></circle>
+        <path
+          class="opacity-75"
+          fill="currentColor"
+          d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+        ></path>
+      </svg>
+      <span v-if="!loading"><slot /></span>
+    </div>
   </button>
 </template>
 
-<script>
-export default {
-  name: 'BaseButton',
-  props: {
-    type: {
-      type: String,
-      default: 'button',
-    },
-    variant: {
-      type: String,
-      default: 'primary',
-    },
-    disabled: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  computed: {
-    computedClass() {
-      return [
-        'px-4 py-2 rounded focus:outline-none focus:ring',
-        {
-          'bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300':
-            this.variant === 'primary',
-          'bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-300':
-            this.variant === 'secondary',
-          'bg-red-500 text-white hover:bg-red-600 focus:ring-red-300':
-            this.variant === 'danger',
-        },
-        { 'opacity-50 cursor-not-allowed': this.disabled },
-      ]
-    },
-  },
-  methods: {
-    onClick(event) {
-      if (!this.disabled) {
-        this.$emit('click', event)
-      }
-    },
-  },
-}
-</script>
+<script setup>
+import { computed } from 'vue'
 
-<style scoped>
-/* Additional styles if needed */
-</style>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'button',
+  },
+  variant: {
+    type: String,
+    default: 'primary',
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits(['click'])
+
+const handleClick = (event) => {
+  if (!props.loading && !props.disabled) {
+    emit('click', event) // Emit click event only if not loading or disabled
+  }
+}
+
+const buttonClasses = computed(() => {
+  const baseClasses =
+    'px-4 py-2 rounded focus:outline-none focus:ring flex items-center justify-center transition duration-300 ease-in-out'
+
+  const variantClasses = (() => {
+    switch (props.variant) {
+      case 'primary':
+        return 'bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300'
+      case 'secondary':
+        return 'bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-300'
+      case 'primaryOutlined':
+        return 'border text-blue-500 hover:bg-blue-100'
+      case 'danger':
+        return 'bg-red-500 text-white hover:bg-red-600 focus:ring-red-300'
+      default:
+        return ''
+    }
+  })()
+
+  const loadingClasses = props.loading ? 'opacity-70 cursor-not-allowed' : ''
+  const disabledClasses = props.disabled ? 'opacity-50 cursor-not-allowed' : ''
+
+  return `${baseClasses} ${variantClasses} ${loadingClasses} ${disabledClasses}`
+})
+</script>
